5d59d5387a3897a7e7465a223424cd725b724b42
Tim <90668895+KrombopulosM16@users.noreply.github.com>
2024-04-02 14:59:04 -0400

    more form processing and get/post mapping

--- src/main/java/com/example/thymeleafdemo/controller/HelloWorldController.java
+++ src/main/java/com/example/thymeleafdemo/controller/HelloWorldController.java
@@ -1,12 +1,18 @@
 package com.example.thymeleafdemo.controller;
 
+
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloWorldController {
     // need a controller method to show initial HTML form
-    @RequestMapping("/showForm")
+    @GetMapping("/showForm")
     public String showForm() {
         return "helloworld-form";
     }
@@ -16,4 +22,63 @@ public class HelloWorldController {
     public String processForm() {
         return "helloworld";
     }
+
+    /*
+        Here we are taking that studentName and making it all upper case. So we are reading
+        form data and calling methods on it.
+    */
+    @RequestMapping("/processFormVersionTwo")
+    //HttpServletRequest lets us read the form data in our controller code
+    public String letsShoutDude(HttpServletRequest request, Model model) {
+        //read the request parameter from the HTML form
+        String theName = request.getParameter("studentName");
+
+        // convert the data to all caps
+        theName = theName.toUpperCase();
+
+        //create the message
+        String result = "Yo! " + theName;
+
+        //add message to the model
+        model.addAttribute("message", result);
+
+        return "helloworld";
+    }
+
+
+    /*
+    Another way of doing this is by binding data to variables using the @RequestParam annotation
+    here we can bind studentName to theName without having to everything we did above.
+     */
+    @PostMapping("/processFormVersionThree")
+    public String letsShoutDude(@RequestParam("studentName") String theName, Model model) {
+        // convert the data to all caps
+        theName = theName.toUpperCase();
+
+        //create the message
+        String result = "Yo! " + theName;
+
+        //add message to the model
+        model.addAttribute("message", result);
+
+        return "helloworld";
+    }
+
+    /*
+    @RequestMapping is a catch-all for http requests. @GetMapping and @PostMapping are examples of
+    more specific mapping for methods. We can use @GetMapping(path="...", method=RequestMethod.GET)
+    so that this method only handles GET requests and will reject everything else.
+    Note: in post requests, the data is sent in the body of the http request message
+    GET - good for:
+    -debugging
+    -bookmarking and email URL
+    -limited data length
+
+    POST - good for:
+    -can't bookmark or email URL
+    -no limitations on data length
+    -can also use binary data
+     */
+
+
 }

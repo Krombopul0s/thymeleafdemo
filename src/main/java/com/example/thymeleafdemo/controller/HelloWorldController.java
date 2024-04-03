4e6be3443ae2d3461484c0a2b6132460f5266b47
Tim <90668895+KrombopulosM16@users.noreply.github.com>
2024-04-02 21:29:42 -0400

    added student form, inputs, and drop down.

--- src/main/java/com/example/thymeleafdemo/controller/HelloWorldController.java
+++ src/main/java/com/example/thymeleafdemo/controller/HelloWorldController.java
@@ -4,10 +4,7 @@ package com.example.thymeleafdemo.controller;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class HelloWorldController {
@@ -79,6 +76,5 @@ public class HelloWorldController {
     -no limitations on data length
     -can also use binary data
      */
-
 
 }
---
+++ src/main/java/com/example/thymeleafdemo/controller/StudentController.java
@@ -0,0 +1,46 @@
+package com.example.thymeleafdemo.controller;
+
+import com.example.thymeleafdemo.model.Student;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
+
+@Controller
+public class StudentController {
+
+    /*
+    This @Value annotation will inject data from application.properties file. The reference tells it what data to use.
+    Our list will be split on the commas and injected.
+     */
+    @Value("${countries}")
+    private List<String> countries;
+
+    @GetMapping("/showStudentForm")
+    public String showForm(Model theModel) {
+        //create Student object
+        Student theStudent = new Student ();
+
+        //add student object to the model
+        theModel.addAttribute("student", new Student());
+
+        //add the list of countries to the model
+        theModel.addAttribute("countries", countries);
+
+        return "student-form";
+    }
+
+    @PostMapping("/processStudentForm")
+    public String processForm(@ModelAttribute("student") Student theStudent) {
+        //log the input data
+        System.out.println("theStudent: " + theStudent.getFirstName()
+                + " " + theStudent.getLastName());
+
+        return "student-confirmation";
+    }
+
+}
